4e38e535d752897fd6a0954f7a5929acaff61592
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-07-29 21:51:46 +0300

    add hero initial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroHill from '../components/home/HeroHill'
+import Hero from '../components/home/Hero'
 
 const Home = () => {
   return (
     <>
       <HeroHill/>
+      <Hero/>
     </>
   )
 }
