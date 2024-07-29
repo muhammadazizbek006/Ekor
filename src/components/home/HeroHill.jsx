913034d9a7a7fda8b9c3d33522695be603dd5dc1
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-07-29 20:32:46 +0300

    addd new component

--- src/components/home/HeroHill.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HeroHill = () => {
-  return (
-    <div>HeroHill</div>
-  )
-}
-
-export default HeroHill
---
+++ src/components/home/ХолмГероев.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ХолмГероев = () => {
+  return (
+    <div>ХолмГероев</div>
+  )
+}
+
+export default ХолмГероев
