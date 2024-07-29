6513ad50277ca42855ff86024164633bdf479619
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-07-29 21:49:33 +0300

    add hero basic

---
+++ src/components/home/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section>
+        <div className="containerb">
+            
+        </div>
+    </section>
+  )
+}
+
+export default Hero
