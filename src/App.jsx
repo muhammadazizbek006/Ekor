540e8f727d78248fc7bb31dd3f06bf783e8d3a0f
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-07-29 10:31:10 +0300

    new download

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import Home from './pages/Home'
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout/>}>
+        <Route index element={<Home/>}/>
+      </Route>
+    )
+  )
   return (
-    <div>App</div>
+    <RouterProvider router={router}/>
   )
 }
 
