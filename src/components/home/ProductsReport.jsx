a9db5e0424fb2385a00011fe548cb73acea839d9
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-07-30 15:48:39 +0300

    add

--- src/components/home/ProductsReport.jsx
+++ src/components/home/ProductsReport.jsx
@@ -10,15 +10,15 @@ const ProductsReport = () => {
             {
               Productsreport.map((e)=>{
                 return(
-                    <li key={e.id} className='shadow-xl  drop-shadow-xl h-56 py-5 px-5 z-50 rounded-2xl '>
+                    <li key={e.id} className='shadow-xl  drop-shadow-xl h-56 py-5 px-4 lg:px-5 z-50 rounded-2xl '>
                         <Link className='relative flex flex-col justify-between   min-h-[170px]  '>
                             {/* left top */}
                             <div className='max-w-[326px] mb-8'>
-                                <h3 className='text-2xl font-semibold text-head leading-7 mb-1.5'>{e.title}</h3>
+                                <h3 className=' md:text-xl xl:text-2xl font-semibold text-head leading-7 mb-1.5'>{e.title}</h3>
                                 <p className='text-sm font-medium text-head leading-[18px] max-w-[181px]     '>{e.sharx}</p>
                             </div>
                             {/* bottom */}
-                            <div className='flex  items-center '>
+                            <div className='flex  items-center justify-between '>
                                 <button className=''>
                                     <img src={e.btn} alt="" />
                                 </button>
