cf76373c6d1a2d42a59751848aa14920f15b9ec5
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-08-01 14:13:15 +0300

    +

---
+++ src/components/home/ProductsOffer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProductsOffer = () => {
+  return (
+    <>
+    {/* ProductsOffer  */}
+        <section>
+            <div className="containerb">
+                <h2>Компания предлагает более 500 наименований рыбной продукции и разнообразных товаров питания</h2>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default ProductsOffer
--- src/components/home/ProductsReport.jsx
+++ src/components/home/ProductsReport.jsx
@@ -10,20 +10,20 @@ const ProductsReport = () => {
             {
               Productsreport.map((e)=>{
                 return(
-                    <li key={e.id} className='shadow-xl drop-shadow-xl h-56 py-5 px-4 lg:px-5 z-50 rounded-2xl relative'>
-                    <Link className='relative flex flex-col justify-between min-h-[170px] '>
+                    <li key={e.id} className='shadow-xl drop-shadow-xl h-56 pt-5 px-4 lg:px-5  rounded-2xl relative'>
+                    <Link className='relative flex flex-col justify-between min-h-[205px] overflow-hidden '>
                         {/* left top */}
                         <div className='max-w-[326px] mb-8'>
                             <h3 className='md:text-xl xl:text-2xl font-semibold text-head leading-7 mb-1.5'>{e.title}</h3>
                             <p className='text-sm font-medium text-head leading-[18px] max-w-[181px]'>{e.sharx}</p>
                         </div>
                         {/* bottom */}
-                        <div className='flex items-center z-0 justify-between'>
-                            <button>
+                        <div className='flex items-center  z-0 justify-between'>
+                            <button className='absolute top-[154px]'>
                                 <img src={e.btn} alt="" />
                             </button>
                             {/* img */}
-                            <div className='absolute right-0 top-14'>
+                            <div className='absolute right-0  z-10 top-16'>
                                 <img src={e.img} alt="" />
                             </div>
                         </div>
