61122660049d18db0930d47204331b6f3bdcee2f
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-08-09 21:37:00 +0300

    a

--- src/components/Contact/EkorAddresses.jsx
+++ src/components/Contact/EkorAddresses.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const MapComponent = () => {
   return (
-    <div>
-        
-    </div>
+        <section>
+            <div className="containerb">
+                
+            </div>
+        </section>
   );
 };
 
