cce67254db7ed736ec929897f8987e14ab4da1e5
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-08-16 17:11:46 +0300

    add new page

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Detail = () => {
+  return (
+    <section>
+        <div className="containerb">
+            
+        </div>
+    </section>
+  )
+}
+
+export default Detail
